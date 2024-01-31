e29e2ab72ceca3510bc695b72c730ca1dd3450af
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2024-01-31 15:28:35 -0300

    Initial Commit

---
+++ ProductCard/ProductCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCardApp.swift
+//  ProductCard
+//
+//  Created by Dmitrii Melnikov on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProductCardView()
+        }
+    }
+}
---
+++ ProductCard/ProductCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCardView.swift
+//  ProductCard
+//
+//  Created by Dmitrii Melnikov on 31.01.2024.
+//
+
+import SwiftUI
+
+struct ProductCardView: View {
+    var body: some View {
+        Image(systemName: "checkMark")
+    }
+}
+
+#Preview {
+    ProductCardView()
+}
